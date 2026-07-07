0e5af1e8834cf3b1b09334095050643c0465d266
milshake301 <gabriel.pecanha.correa16@gmail.com>
2026-07-06 21:07:22 -0300

    prog26.py

--- prog26.py
+++ prog26.py
@@ -1,6 +1,6 @@
-n1 = float(input("coloque um número"))
-n2 = float(input("coloque um número"))
+n1 = float(input("coloque um número "))
+n2 = float(input("coloque um número "))
 if n1>n2:
-    print(f"o valor: {n1} é maior que {n2}")
+    print(f"o valor de: {n1} é maior que {n2} ")
 else:
-    print(f"o valor: {n2} é maior que {n1}")
+    print(f"o valor de: {n2} é maior que {n1} ")
