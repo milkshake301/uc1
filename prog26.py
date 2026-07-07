0d1d60759d57529cc32ec3b206f67affc77ec527
milkshake301 <gabriel.pecanha.correa16@gmail.com>
2026-07-06 21:02:53 -0300

    Update prog26.py

--- prog26.py
+++ prog26.py
@@ -1,6 +1,7 @@
-n1 = float(input("coloque um número"))
-n2 = float(input("coloque um número"))
+n1 = float(input("coloque um número "))
+n2 = float(input("coloque um número "))
 if n1>n2:
-    print(f"o valor: {n1} é maior que {n2}")
+    print(f"o valor: {n1} é maior que {n2} ")
 else:
-    print(f"o valor: {n2} é maior que {n1}")
+    print(f"o valor: {n2} é maior que {n1} ")
+print("Teste de pool")
