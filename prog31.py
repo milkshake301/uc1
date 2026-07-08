32186dd8d39e512c9b735854e170ea0483295563
milshake301 <gabriel.pecanha.correa16@gmail.com>
2026-07-08 20:29:30 -0300

    progs

--- prog31.py
+++ prog31.py
@@ -1,3 +1,3 @@
 nome= input("Seu nome")
 for i in range(1,6):
-    print(f"{i} {nome} ")
+    print(f" {nome} ")
---
+++ prog34.py
@@ -0,0 +1,6 @@
+m=0
+x=int(input("digite um valor "))
+m = m + x
+print(f"a tabuada de e: {m} ")
+for i in range(1,11):
+    print(f"{m} x {i} = {m * i}")
